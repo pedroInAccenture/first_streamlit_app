8cb5ea3dc19af04e912eeded238a911ad542629e
pedroInAccenture <112963398+pedroInAccenture@users.noreply.github.com>
2022-09-30 19:33:58 -0500

    Update streamlit_app.py
    se agrega texto sugerido

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title("Title")
-
-streamlit.header("HOLA")
-
-streamlit.text("HOLA")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
